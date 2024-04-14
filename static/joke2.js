627ef8dfad2fc0239ccb5be6f57c4caf3af8c5e8
Teoh TeikToe <teohteiktoe@gmail.com>
2024-04-14 09:17:22 +0800

    Add files via upload

---
+++ static/joke2.js
@@ -0,0 +1,6 @@
+function joke2(){
+    alert(
+        "What did one Singaporean say to the other in a crowded hawker centre?"+
+        "Can you please chope that seat with chili sauce instead of tissue?"
+    )
+}
